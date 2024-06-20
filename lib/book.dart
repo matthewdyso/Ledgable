65a916691dccff3f091d9be6a362420e7aec8b9f
Matthew So <samurai.ms12@gmail.com>
2024-06-20 13:56:04 -0400

    Two book styles

--- lib/book.dart
+++ lib/book.dart
@@ -1,84 +1,138 @@
 import 'package:flutter/material.dart';
 
-class Book extends StatelessWidget{
-  String title;
-  String summary;
+// class Book extends StatelessWidget{
+//   final String title;
+//   final String summary;
+//
+//   Book(this.title, this.summary);
+//
+//   String getTitle() {
+//     return title;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Book Widget Example'),
+//         ),
+//         body: Center(
+//           child: Container(
+//             width: 50,
+//             height: 150,
+//             decoration: BoxDecoration(
+//               color: Colors.white,
+//               borderRadius: BorderRadius.circular(10),
+//               boxShadow: [
+//                 BoxShadow(
+//                   color: Colors.grey.withOpacity(0.5),
+//                   spreadRadius: 2,
+//                   blurRadius: 5,
+//                   offset: Offset(0, 3),
+//                 ),
+//               ],
+//             ),
+//             child: Stack(
+//               children: [
+//                 Positioned(
+//                   left: 0,
+//                   top: 0,
+//                   bottom: 0,
+//                   child: Container(
+//                     width: 50,
+//                     decoration: BoxDecoration(
+//                       color: Colors.blue,
+//                       borderRadius: BorderRadius.only(
+//                         topLeft: Radius.circular(10),
+//                         bottomLeft: Radius.circular(10),
+//                         topRight: Radius.circular(10),
+//                         bottomRight: Radius.circular(10),
+//                       ),
+//                     ),
+//                     child: Center(
+//                       child: RotatedBox(
+//                         quarterTurns: 3,
+//                         child: Text(
+//                           this.title,
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
-  Book(this.title, this.summary);
 
-  String getTitle() {
-    return title;
-  }
+
+class Book extends StatelessWidget {
+  final String title;
+
+  Book(this.title);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Book Widget Example'),
-        ),
-        body: Center(
-          child: Container(
-            width: 50,
-            height: 150,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        width: 50,
+        height: 150,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
             ),
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 0,
-                  top: 0,
-                  bottom: 0,
-                  child: Container(
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                    ),
-                    child: Center(
-                      child: RotatedBox(
-                        quarterTurns: 3,
-                        child: Text(
-                          this.title,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: RotatedBox(
+                    quarterTurns: -3,
+                    child: Text(
+                      this.title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
 }
 
-
-//Test code
-
 void main() {
-  runApp(Book("idk","summary"));
-  runApp(Book("cool","summary"));
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Book('My Book Title'),
+      ),
+    ),
+  ));
 }
