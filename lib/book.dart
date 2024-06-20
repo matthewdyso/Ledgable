04bbe1c605b5fdb5b4fafc53198aa64acf454519
Matthew So <samurai.ms12@gmail.com>
2024-06-20 13:48:14 -0400

    Book Shape and Design

--- lib/book.dart
+++ lib/book.dart
@@ -1,15 +1,84 @@
-class Book{
+import 'package:flutter/material.dart';
+
+class Book extends StatelessWidget{
   String title;
   String summary;
+
 
   Book(this.title, this.summary);
 
   String getTitle() {
     return title;
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Book Widget Example'),
+        ),
+        body: Center(
+          child: Container(
+            width: 50,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: RotatedBox(
+                        quarterTurns: 3,
+                        child: Text(
+                          this.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
+
+//Test code
+
 void main() {
-    Book idk = Book("random title", "random summary");
-    print(idk.getTitle());
+  runApp(Book("idk","summary"));
+  runApp(Book("cool","summary"));
 }
