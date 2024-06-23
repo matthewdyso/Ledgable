1bdcb14d3513dd383c06148aeea990722aa721bd
Matthew So <samurai.ms12@gmail.com>
2024-06-23 15:03:43 -0400

    Multiple books created with position customization.

--- lib/book.dart
+++ lib/book.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:ui';
 // class Book extends StatelessWidget{
 //   final String title;
 //   final String summary;
@@ -75,52 +75,56 @@ import 'package:flutter/material.dart';
 
 
 class Book extends StatelessWidget {
-  final String title;
-  final String summary;
-  final String author;
-  Book(this.title, this.author, this.summary);
+  String title;
+  String summary;
+  String author;
+  double y;
+  double x;
+
+  Book(this.title, this.author, this.summary, this.x, this.y);
+
+  void set(double xOffset, double yOffset) {
+    x = xOffset;
+    y = yOffset;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container(
-        width: 50,
-        height: 150,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(0, 3),
+    return Stack(
+      children: [
+        Positioned(
+          top: y,
+          left: x,
+          child: Container(
+            width: 50,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Container(
-                color: Colors.blue,
-                child: Center(
-                  child: RotatedBox(
-                    quarterTurns: -3,
-                    child: Text(
-                      this.title,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+            child: Center(
+              child: RotatedBox(
+                quarterTurns: -3,
+                child: Text(
+                  this.title,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
@@ -130,18 +134,13 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Center(
-        child: Book('Harry Potter and the Order of the Phoenix',
-            'J. K. Rowling', 'Now in his fifth year at Hogwarts, '
-            'Harry (Daniel Radcliffe) learns that many in the '
-            'wizarding community do not know the truth of his '
-            'encounter with Lord Voldemort. Cornelius Fudge, '
-            'minister of Magic, appoints his toady, Dolores '
-            'Umbridge, as Defense Against the Dark Arts teacher'
-            ', for he fears that professor Dumbledore will take'
-            ' his job. But her teaching is deficient and her '
-            'methods, cruel, so Harry prepares a group of '
-            'students to defend the school against a rising '
-            'tide of evil.'),
+        child: Stack(
+          children: [
+            Book('Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 'massive yap', 10, 100),
+            Book('Game of Thrones', 'George RR Martin', 'massive yap', 70, 100),
+            Book('IDK anymore', 'J. K. Rowling', 'massive yap', 130, 100),
+          ],
+        ),
       ),
     ),
   ));
