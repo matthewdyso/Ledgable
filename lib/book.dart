1239bc9890bb398819660e5dd3778bbf151bd304
Matthew So <samurai.ms12@gmail.com>
2024-06-24 15:30:22 -0400

    Book class and BookUI class

--- lib/book.dart
+++ lib/book.dart
@@ -1,80 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
-// class Book extends StatelessWidget{
-//   final String title;
-//   final String summary;
-//
-//   Book(this.title, this.summary);
-//
-//   String getTitle() {
-//     return title;
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Book Widget Example'),
-//         ),
-//         body: Center(
-//           child: Container(
-//             width: 50,
-//             height: 150,
-//             decoration: BoxDecoration(
-//               color: Colors.white,
-//               borderRadius: BorderRadius.circular(10),
-//               boxShadow: [
-//                 BoxShadow(
-//                   color: Colors.grey.withOpacity(0.5),
-//                   spreadRadius: 2,
-//                   blurRadius: 5,
-//                   offset: Offset(0, 3),
-//                 ),
-//               ],
-//             ),
-//             child: Stack(
-//               children: [
-//                 Positioned(
-//                   left: 0,
-//                   top: 0,
-//                   bottom: 0,
-//                   child: Container(
-//                     width: 50,
-//                     decoration: BoxDecoration(
-//                       color: Colors.blue,
-//                       borderRadius: BorderRadius.only(
-//                         topLeft: Radius.circular(10),
-//                         bottomLeft: Radius.circular(10),
-//                         topRight: Radius.circular(10),
-//                         bottomRight: Radius.circular(10),
-//                       ),
-//                     ),
-//                     child: Center(
-//                       child: RotatedBox(
-//                         quarterTurns: 3,
-//                         child: Text(
-//                           this.title,
-//                           style: TextStyle(
-//                             color: Colors.white,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
-
-class Book extends StatelessWidget {
+class Book{
   String title;
   String summary;
   String author;
@@ -83,18 +9,38 @@ class Book extends StatelessWidget {
 
   Book(this.title, this.author, this.summary, this.x, this.y);
 
-  void set(double xOffset, double yOffset) {
-    x = xOffset;
-    y = yOffset;
+  // Setter methods for updating properties
+  void setTitle(String newTitle) {
+    title = newTitle;
   }
+
+  void setSummary(String newSummary) {
+    summary = newSummary;
+  }
+
+  void setAuthor(String newAuthor) {
+    author = newAuthor;
+  }
+
+  void setPosition(double newX, double newY) {
+    x = newX;
+    y = newY;
+  }
+
+}
+
+class BookUI extends StatelessWidget {
+  final Book bookData;
+
+  const BookUI(this.bookData, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Positioned(
-          top: y,
-          left: x,
+          top: bookData.y,
+          left: bookData.x,
           child: Container(
             width: 50,
             height: 150,
@@ -106,7 +52,7 @@ class Book extends StatelessWidget {
                   color: Colors.grey.withOpacity(0.5),
                   spreadRadius: 2,
                   blurRadius: 5,
-                  offset: Offset(0, 3),
+                  offset: const Offset(0, 3),
                 ),
               ],
             ),
@@ -114,8 +60,8 @@ class Book extends StatelessWidget {
               child: RotatedBox(
                 quarterTurns: -3,
                 child: Text(
-                  this.title,
-                  style: TextStyle(
+                  bookData.title,
+                  style: const TextStyle(
                     color: Colors.blue,
                     fontWeight: FontWeight.bold,
                   ),
@@ -131,15 +77,21 @@ class Book extends StatelessWidget {
 
 
 void main() {
+
+  Book harryPotter = Book('Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 'massive yap', 10, 100);
+  Book got = Book('Game of Thrones', 'George RR Martin', 'massive yap', 70, 100);
+  Book idk = Book('IDK anymore', 'J. K. Rowling', 'massive yap', 130, 100);
+  Book random = Book('Random Book', 'J. K. Rowling', 'massive yap', 190, 100);
+
   runApp(MaterialApp(
     home: Scaffold(
       body: Center(
         child: Stack(
           children: [
-            Book('Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 'massive yap', 10, 100),
-            Book('Game of Thrones', 'George RR Martin', 'massive yap', 70, 100),
-            Book('IDK anymore', 'J. K. Rowling', 'massive yap', 130, 100),
-            Book('Random Book', 'J. K. Rowling', 'massive yap', 190, 100),
+            BookUI(harryPotter),
+            BookUI(got),
+            BookUI(idk),
+            BookUI(random),
           ],
         ),
       ),
