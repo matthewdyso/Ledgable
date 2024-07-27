11d717abc9549aaf29e97d5fa93d4c74a287852b
matthewso146 <matthewso146@gmail.com>
2024-07-27 02:40:58 -0400

    sortBooks function fix and removal of extra code

--- lib/shelf.dart
+++ lib/shelf.dart
@@ -72,11 +72,11 @@ class Shelf {
 
 
   int sortBooks(final Book a, final Book b, int selection){
-    switch(selection) {
+    switch (selection) {
       case 1: // Newest to oldest
-        return a.date.compareTo(b.date); // Compare dates
-      case 2: // Oldest to newest
         return b.date.compareTo(a.date); // Compare dates
+      case 2: // Oldest to newest
+        return a.date.compareTo(b.date); // Compare dates
       case 3: // Title A-Z
         return a.title.compareTo(b.title);
       case 4: // Title Z-A
@@ -202,21 +202,21 @@ class ShelfApp extends StatelessWidget {
   5 = Author A-Z
   6 = Author Z-A
 */
-int sortBooks(final Book a, final Book b, int selection) {
-  switch (selection) {
-    case 1: // Newest to oldest
-      return b.date.compareTo(a.date); // Compare dates
-    case 2: // Oldest to newest
-      return a.date.compareTo(b.date); // Compare dates
-    case 3: // Title A-Z
-      return a.title.compareTo(b.title);
-    case 4: // Title Z-A
-      return b.title.compareTo(a.title);
-    case 5: // Author A-Z
-      return a.author.compareTo(b.author);
-    case 6: // Author Z-A
-      return b.author.compareTo(a.author);
-    default:
-      return 0;
-  }
-}
+// int sortBooks(final Book a, final Book b, int selection) {
+//   switch (selection) {
+//     case 1: // Newest to oldest
+//       return b.date.compareTo(a.date); // Compare dates
+//     case 2: // Oldest to newest
+//       return a.date.compareTo(b.date); // Compare dates
+//     case 3: // Title A-Z
+//       return a.title.compareTo(b.title);
+//     case 4: // Title Z-A
+//       return b.title.compareTo(a.title);
+//     case 5: // Author A-Z
+//       return a.author.compareTo(b.author);
+//     case 6: // Author Z-A
+//       return b.author.compareTo(a.author);
+//     default:
+//       return 0;
+//   }
+// }
