a5f162b07137cd272b85b31cef7fd29e7e92775a
Matthew So <samurai.ms12@gmail.com>
2024-08-07 19:21:22 -0400

    updated to be more like main

--- lib/main.dart
+++ lib/main.dart
@@ -130,3 +130,68 @@ class ledgableAppState extends State<ledgableApp> {
   }
 }
 
+
+/*
+
+import 'package:flutter/material.dart';
+import 'package:ledgable/models/book.dart';
+import 'package:ledgable/models/shelf.dart';
+import 'package:ledgable/widgets/edit_book_dialog.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+>>>>>>> Stashed changes
+
+//Stateless widget does not change, basically the background of the app
+class ledgableApp extends StatelessWidget {
+  const ledgableApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Book harryPotter = Book('Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 'He said calmly');
+    Book got = Book('Game of Thrones', 'George RR Martin', 'Bilbo Baggins');
+    Book idk = Book('IDK anymore', 'J. K. Rowling', 'IDK man this aint a book');
+    Book random = Book('Random Book', 'J. K. Rowling', 'probability of me being a book = 0');
+
+    void handleBookPress(Book book){
+      print(book.summary);
+    }
+
+    return MaterialApp(
+      title: 'ledgable',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const SizedBox(
+            width: 50, // Adjust width as needed
+            height: 50, // Adjust height as needed
+            child: Image(
+              image: AssetImage('assets/ledgable_icon.png'),
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Stack(
+            children: [
+              // x increments by values of 60
+              BookUI(harryPotter, 5, 100, onPress: () => handleBookPress(harryPotter)),
+              BookUI(got, 65, 100, onPress: () => handleBookPress(got)),
+              BookUI(idk, 125, 100, onPress: () => handleBookPress(idk)),
+              BookUI(random, 185, 100, onPress: () => handleBookPress(random)),
+              BookUI(harryPotter, 245, 100, onPress: () => handleBookPress(harryPotter)),
+              BookUI(harryPotter, 305, 100, onPress: () => handleBookPress(harryPotter)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const ledgableApp());
+}
+
+ */
--- lib/models/shelf.dart
+++ lib/models/shelf.dart
@@ -3,9 +3,18 @@ import 'package:ledgable/models/book.dart';
 // Shelf class to hold and manage a collection of books of the same size
 class Shelf {
   List<Book> books = [];
+  double initY = 30;
+  double width;
+  double height;
 
   // Constructor to initialize shelf dimensions
-  Shelf();
+  Shelf({this.width = 0, this.height = 0});
+
+  // Method to set shelf dimensions
+  void setSize(double w, double h) {
+    width = w;
+    height = h;
+  }
 
   // Method to add a book to the shelf
   void addBook(Book book) {
